e63f62b4f0b895359f681c9f6a6e6d9e36edc39b
Sawant Kumar <sawant_kumar@condenast.com>
2022-09-08 19:04:26 +0530

    Initial Commit

---
+++ UniverseGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniverseGallery
+//
+//  Created by Kumar, Sawant on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
